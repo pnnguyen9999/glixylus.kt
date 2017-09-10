37b6e47bb191e877123256e45b324651d1562c09
pnnguyen9999 <31655664+pnnguyen9999@users.noreply.github.com>
2017-09-10 23:01:34 +0700

    cập nhật file view.

--- resources/views/test1.blade.php
+++ resources/views/test1.blade.php
@@ -7,7 +7,6 @@
 <!-- BOOTSTRAP -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="{{ URL::asset('https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js') }}"></script>
 <script type="text/javascript" src="{{ URL::asset('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js') }}"></script>
 <!-- CUSTOM CSS -->
@@ -16,18 +15,19 @@
 <!--use laravel format to import css & js file-->
 <body style='font-family: font1;'>
 
-<div class="col-sm-12" style="height: 55px; border-top: 6px solid #00E794;border-bottom: 1px solid #D4D4D4">
+<div class="col-sm-12" style="height: 50px; border-top: 6px solid #00E794;border-bottom: 1px solid #D4D4D4">
  
-  <div class="col-sm-1" style="float: left;background-color: #E0E0E0;height: 100%">
+  <div class="col-sm-1" style="float: left;background-color: #ECECEC;height: 100%">
   
   </div>
-  <div class="col-sm-3" style="float: left;height: 100%;text-align:center;">
-  	<input type="text" class="timkiem" style="height: 100%;" placeholder="&nbsp; tìm kiếm . . .">
+  <div class="col-sm-2" style="float: left;height: 100%;text-align:center;">
+  	<input type="text" class="timkiem" style="height: 100%;outline: none;" placeholder="&nbsp; tìm kiếm . . .">
   </div>
-  <div class="col-sm-6" style="float: left;height: 100%">
+  
+  <div class="col-sm-8" style="float: left;height: 100%;margin-top: 3px;">
   <div class="">
-  	<div class="dropdown col-sm-3" style="float:left">
-		<button class=" dropdown-toggle dropstyle" type="button" data-toggle="dropdown">Mục 1
+  	<div class="dropdown" style="width: 25%; float:left;">
+		<button class=" dropdown-toggle dropstyle" type="button" data-toggle="dropdown">công trình
 		<span class="caret"></span></button>
 		<ul class="dropdown-menu" style="font-size: 13pt;">
 		  <li><a href="#">Mục con 1</a></li>
@@ -38,8 +38,8 @@
 		  <li><a href="#">Mục con 3</a></li>
 		</ul>
     </div>
-      <div class="dropdown col-sm-3" style="float:left">
-		<button class=" dropdown-toggle dropstyle" type="button" data-toggle="dropdown">Mục 2
+      <div class="dropdown " style="width: 28%;float:left">
+		<button class=" dropdown-toggle dropstyle" type="button" data-toggle="dropdown">thiết kế đề xuất
 		<span class="caret"></span></button>
 		<ul class="dropdown-menu" style="font-size: 13pt;">
 		  <li><a href="#">Mục con 1</a></li>
@@ -50,40 +50,23 @@
 		  <li><a href="#">Mục con 3</a></li>
 		</ul>
     </div>
-    <div class="dropdown col-sm-3" style="float:left">
-		<button class=" dropdown-toggle dropstyle" type="button" data-toggle="dropdown">Mục 3
-		<span class="caret"></span></button>
-		<ul class="dropdown-menu" style="font-size: 13pt;">
-		  <li><a href="#">Mục con 1</a></li>
-		  <li><a href="#">Mục con 2</a></li>
-		  <li><a href="#">Mục con 3</a></li>
-		  <li><a href="#">Mục con 1</a></li>
-		  <li><a href="#">Mục con 2</a></li>
-		  <li><a href="#">Mục con 3</a></li>
-		</ul>
+    <div class="dropdown " style="width: 20%;float:left">
+		<button class=" dropdown-toggle dropstyle" type="button" data-toggle="dropdown">xu hướng
+		
     </div>
     
-    <div class="dropdown col-sm-3" style="float:left">
-		<button class=" dropdown-toggle dropstyle" type="button" data-toggle="dropdown">Mục 4
-		<span class="caret"></span></button>
-		<ul class="dropdown-menu" style="font-size: 13pt;">
-		  <li><a href="#">Mục con 1</a></li>
-		  <li><a href="#">Mục con 2</a></li>
-		  <li><a href="#">Mục con 3</a></li>
-		  <li><a href="#">Mục con 1</a></li>
-		  <li><a href="#">Mục con 2</a></li>
-		  <li><a href="#">Mục con 3</a></li>
-		</ul>
+    <div class="dropdown " style="width: 15%;float:left">
+		<button class=" dropdown-toggle dropstyle" >liên hệ
     </div>
     </div>
   </div>
-   <div class="col-sm-2" style="float: left;height: 100%; border-right: 1px solid #E5E5E5;border-left: 1px solid #E5E5E5">
+   <div class="col-sm-1" style="float: left;height: 100%; border-right: 1px solid #E5E5E5;border-left: 1px solid #E5E5E5">
   	
   </div>
 
 </div>
 
-<div class="col-sm-12" style="height: 250px;text-align: center;border-bottom: 1px solid #ADADAD">
+<div class="col-sm-12 w3-card-2" style="height: 250px;text-align: center;border-bottom: 1px solid #ADADAD;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 6px 20px 0 rgba(0, 0, 0, 0.1);">
 	
 	
 	
@@ -102,7 +85,7 @@
 		<div class="col-md-3 col-sm-6 col-xs-6" style="padding: 5px;">
 			<div class="gallery">
 				  <a target="_blank" href="img_fjords.jpg">
-					<img src="/img/2.png" alt="Trolltunga Norway"  height="200">
+					<img src="/img/a.png" alt="Trolltunga Norway"  height="200">
 				  </a>
 				  
 			</div>
@@ -110,7 +93,7 @@
 		<div class="col-md-3 col-sm-6 col-xs-6" style="padding: 5px;">
 			<div class="gallery">
 				  <a target="_blank" href="img_fjords.jpg">
-					<img src="/img/2.png" alt="Trolltunga Norway"  height="200">
+					<img src="/img/b.png" alt="Trolltunga Norway"  height="200">
 				  </a>
 				 
 			</div>
@@ -118,7 +101,7 @@
 		<div class="col-md-3 col-sm-6 col-xs-6" style="padding: 5px;">
 			<div class="gallery">
 				  <a target="_blank" href="img_fjords.jpg">
-					<img src="/img/2.png" alt="Trolltunga Norway"  height="200">
+					<img src="/img/c.png" alt="Trolltunga Norway"  height="200">
 				  </a>
 				  
 			</div>
@@ -126,11 +109,11 @@
 		<div class="col-md-3 col-sm-6 col-xs-6" style="padding: 5px;">
 			<div class="gallery">
 				  <a target="_blank" href="img_fjords.jpg">
-					<img src="/img/2.png" alt="Trolltunga Norway"  height="200">
+					<img src="/img/d.png" alt="Trolltunga Norway"  height="200">
 				  </a>
 				 
 			</div>
-		</div>d
+		</div>
 		<!-- 
 		-->
 		
@@ -138,26 +121,116 @@
 	<div class="col-sm-2"></div>
 </div>
 
-<div style="height: 580px"></div>
+<div style="height: 570px;">
+	
+</div>
+<div style="text-align: center;">
+	<img src="/img/vectorSpace.png" height="50">
+</div>
+<br>
 <div class="col-sm-12" style="text-align: center;font-family: font2;">
 	
 	<b><h2 style="letter-spacing: 5px;">MUA BÁN & DỊCH VỤ</h2></b>
 	<br>
 </div>
 
+
+
+
+
+
+<!-- lạnh lùng 
+<input type="button" value="chuyen trang" onclick="chuyen_trang()">
+<input type="button" value="xoa cookie" onclick="xoa()">-->
+<!-- lạnh lùng -->
 <div style="padding: 0px;height: 800px"> 
 <div class="col-sm-3" style="border-right: 1px solid #C8C8C8;height: 100%;text-align: center">
-	<h3 style="border-bottom: 3px dashed #00E794;letter-spacing: 5px">MENU</h3>
+	<h3 style="border-bottom: 3px dashed #00E794;letter-spacing: 5px">PHÂN LOẠI</h3>
+
+	  <div class="panel panel-default" style="font-family: font3;margin-bottom:5px">
+		<div class="panel-heading" style="cursor: pointer" data-toggle="collapse" data-target="#itemLoai1">Loại 1<span class="glyphicon glyphicon-menu-down" style="float: right"></span></div>
+			<div id="itemLoai1" class="">
+				<div class="panel-body" style="border-bottom: 1px solid #E3E3E3">itemLoai1</div>
+				<div class="panel-body" style="border-bottom: 1px solid #E3E3E3">itemLoai1</div>
+				<div class="panel-body" style="border-bottom: 1px solid #E3E3E3">itemLoai1</div>
+			</div>
+	  	</div>
+	 <div class="panel panel-default" style="font-family: font3;margin-bottom:5px">
+		<div class="panel-heading" style="cursor: pointer" data-toggle="collapse" data-target="#itemLoai2">Loại 2<span class="glyphicon glyphicon-menu-down" style="float: right"></span></div>
+			<div id="itemLoai2" class="collapse">
+				<div class="panel-body" style="border-bottom: 1px solid #E3E3E3">itemLoai2</div>
+				<div class="panel-body" style="border-bottom: 1px solid #E3E3E3">itemLoai2</div>
+				<div class="panel-body" style="border-bottom: 1px solid #E3E3E3">itemLoai2</div>
+			</div>
+	  	</div>
+	  <div class="panel panel-default" style="font-family: font3;margin-bottom:5px">
+		<div class="panel-heading" style="cursor: pointer" data-toggle="collapse" data-target="#itemLoai3">Loại 3<span class="glyphicon glyphicon-menu-down" style="float: right"></span></div>
+			<div id="itemLoai3" class="collapse">
+				<div class="panel-body" style="border-bottom: 1px solid #E3E3E3">itemLoai3</div>
+				<div class="panel-body" style="border-bottom: 1px solid #E3E3E3">itemLoai3</div>
+				<div class="panel-body" style="border-bottom: 1px solid #E3E3E3">itemLoai3</div>
+			</div>
+	  	</div>
 </div>	
 <div class="col-md-9 col-sm-12 col-xs-12" style="text-align: center" id="div_parent">
 	<h3 style="font-family: font2;letter-spacing: 5px">-SẢN PHẨM-</h3>
-	<script type="text/javascript" src="{{URL::asset('js\Load.js')}}"></script>
+<script type="text/javascript" src="{{URL::asset('js\Load.js')}}"></script>
 	<script type="text/javascript">Load_Product_To_View('{{$data}}');</script>
 	<input type="button" value="chuyen_trang" id="pay_btt">
 	<script type="text/javascript" src="{{URL::asset('js\Product_Activity.js')}}"></script>
+	<div class="col-md-3 col-sm-6 col-xs-6 shopItem" >
+		<div class="gallery" name="san_pham">
+				  <a target="_blank" href="#">
+					<img src="/img/4.png" alt="test"  height="100">
+				  </a>
+				  <div class="desc">
+					  <h4>sản phẩm 1</h4>
+					  <h4 style="font-family: font3">1.400.000 VND</h4>
+						<button type="button" class="btn btn-success" style="width: 80%"><span class="glyphicon glyphicon-shopping-cart"></span>&nbsp; thêm vào giỏ</button>
+				  </div>
+		</div>
+	</div>
+	<div class="col-md-3 col-sm-6 col-xs-6 shopItem" >
+		<div class="gallery" name="san_pham">
+				  <a target="_blank" href="#">
+					<img src="/img/4.png" alt="test"  height="100">
+				  </a>
+				  <div class="desc">
+					  <h4>sản phẩm 1</h4>
+					  <h4 style="font-family: font3">1.400.000 VND</h4>
+						<button type="button" class="btn btn-success" style="width: 80%"><span class="glyphicon glyphicon-shopping-cart"></span>&nbsp; thêm vào giỏ</button>
+				  </div>
+		</div>
+	</div>
+	<div class="col-md-3 col-sm-6 col-xs-6 shopItem" >
+		<div class="gallery" name="san_pham">
+				  <a target="_blank" href="#">
+					<img src="/img/4.png" alt="test"  height="100">
+				  </a>
+				  <div class="desc">
+					  <h4>sản phẩm 1</h4>
+					  <h4 style="font-family: font3">1.400.000 VND</h4>
+						<button type="button" class="btn btn-success" style="width: 80%"><span class="glyphicon glyphicon-shopping-cart"></span>&nbsp; thêm vào giỏ</button>
+				  </div>
+		</div>
+	</div>
+	<div class="col-md-3 col-sm-6 col-xs-6 shopItem" >
+		<div class="gallery" name="san_pham">
+				  <a target="_blank" href="#">
+					<img src="/img/4.png" alt="test"  height="100">
+				  </a>
+				  <div class="desc">
+					  <h4>sản phẩm 1</h4>
+					  <h4 style="font-family: font3">1.400.000 VND</h4>
+						<button type="button" class="btn btn-success" style="width: 80%"><span class="glyphicon glyphicon-shopping-cart"></span>&nbsp; thêm vào giỏ</button>
+				  </div>
+		</div>
+	</div>
 
-</div>
-
+<!--
+<div class="col-sm-3" style="border-left: 1px solid #C8C8C8;height: 100%;text-align: center">
+	<h3 style="border-bottom: 3px dashed #00E794;letter-spacing: 5px">CHI TIẾT</h3>
+</div>	i love you đính -->
 </div>
 
 <div class="col-sm-12" style="width: 100%;height: 26px;border-top: 1px solid #D0D0D0;font-family: font3;text-align: center;position: fixed;bottom: 0px;background-color: #ffffff;border-bottom: 6px solid #21C375">
